9e0a35fe90824b13cc3637a568096f7252951bf1
samwhindleton <samwhindleton@gmail.com>
2018-05-14 06:02:18 -0700

    added classes for styling

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+// components
 import Layout from '../components/Layout';
+
 
 const About = () => (
   <Layout>
-    <div>
-      <h1>About page.</h1>
+    <div className='about'>
+      <p>About</p>
     </div>
   </Layout>
 );
