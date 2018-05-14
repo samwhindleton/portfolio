1f40691356b78f00f9b4312b50260b7dff9f20b8
samwhindleton <samwhindleton@gmail.com>
2018-05-14 06:04:23 -0700

    added classes for styling

--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,11 @@
+// components
 import Layout from '../components/Layout';
+
 
 const Resume = () => (
   <Layout>
-    <div>
-      <h1>R&#233;sum&#233; page.</h1>
+    <div className='resume'>
+      <p>R&#233;sum&#233;</p>
     </div>
   </Layout>
 );
